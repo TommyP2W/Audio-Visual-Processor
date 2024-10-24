7d5f0442f4d6ec7f6a755b89e5bfbc88ca7e85b4
TommyP2W <152071432+TommyP2W@users.noreply.github.com>
2024-10-24 15:30:00 +0100

    AVP start

--- avp2.py
+++ avp2.py
@@ -8,23 +8,24 @@ import soundfile as sf
 import matplotlib.pyplot as plt
 import numpy as np
 ### freq
-fs = 16000
+frequency = 16000
 
 
 ## magSpec calc
 def magAndPhase(SpeechFrame):
-    s = np.squeeze(SpeechFrame)
-    Hamming_window = s*np.hamming(320)
-    Xf = np.fft.fft(Hamming_window)
-    shortTimeMag = np.abs(Xf)
-    shortTimephase= np.angle(Xf)
+    speech = np.squeeze(SpeechFrame)
+    hamming_window = speech*np.hamming(320)
+    fft = np.fft.fft(hamming_window)
+    shortTimeMag = np.abs(fft)
+    shortTimephase = np.angle(fft)
     
     return shortTimeMag, shortTimephase
+
 file_count = 0
 
 ### Loading in all audio files found in directory
 for soundfile in sorted(glob.glob('AVPSAMPLESMONO\\*\\*.wav')):
-    r, fs = sf.read(soundfile)
+    r, frequency = sf.read(soundfile)
     file_count = file_count+1
     frameLength = 320
     numSamples = len(r)
